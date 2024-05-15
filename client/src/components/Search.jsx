4ec465f404e23ba91bdc533ce286792eaf3aa463
Pallavi <pallavi25kishore@gmail.com>
2024-05-15 12:54:55 -0700

    implement level 1

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" className="search-text"></input>
+      <button>Go!</button>
+    </div>
+  );
+};
+
+export default Search;
